2001e538f941c8acba115a37105185a1b9a30f6f
quyhot <quyhot1@gmail.com>
2022-04-14 15:44:01 +0700

    update code

---
+++ index.js
@@ -0,0 +1,5 @@
+a = document.getElementsByClassName("dxichTextCellSys")
+for (let i = 3; i < a.length; i += 5){
+    a[i].click() 
+}
+document.getElementById("ctl00_ctl00_contentPane_MainPanel_MainContent_formLayout_submitButton").click()
